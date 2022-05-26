aa3e3e387bde46772de639d6134571dcf128a2a0
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-05-26 11:30:42 +0200

    added AttackResponse and html styles to eventlistener

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import { Gameboard } from "./Gameboard";
 import { Ship } from "./Ship";
 import { Coordinate } from "./Coordinate";
+import { AttackResponse } from "./AttackResponse";
 //
 // let playerTurn = true;
 // const gameboardPlayer = new Gameboard(10);
@@ -43,33 +44,56 @@ import { Coordinate } from "./Coordinate";
 // }
 //
 
-function initializeGameboard(field: HTMLDivElement) {
-  for (let y = 1; y <= 10; y++) {
-    for (let x = 1; x <= 10; x++) {
+function initializeGameboard(field: HTMLDivElement, gameboard: Gameboard) {
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
       const divElement = document.createElement("div");
       field?.appendChild(divElement);
       divElement.classList.add("element");
       divElement.addEventListener("click", () => {
         const coordinate: Coordinate = { x: x, y: y };
-        if (gameboardPlayer.placedShips.has(JSON.stringify(coordinate))) {
-          divElement.style.backgroundColor = "red";
+        let isHit = gameboard.receiveAttack(coordinate);
+        if (isHit === AttackResponse.Hit) {
+          divElement.innerHTML = "X";
+          divElement.style.color = "red";
+          divElement.style.backgroundColor = "#ffbacd";
+          console.log("x: ", x, "y: ", y);
+          console.log("Ship hit");
+        } else if (isHit === AttackResponse.Sunk) {
+          divElement.innerHTML = "X";
+          divElement.style.color = "red";
+          divElement.style.backgroundColor = "#ffbacd";
+          console.log("Ship sunk");
+        } else {
+          divElement.innerHTML = "O";
+          divElement.style.color = "green";
+          divElement.style.backgroundColor = "#e3f3e7";
+          console.log("Ship missed");
+        }
+        if (gameboard.allShipsSunk()) {
+          console.log("All ships sunk!");
         }
       });
-      console.log("njeirho");
     }
   }
 }
 
+const playerDestroyer = new Ship(2);
+const gameboardPlayer = new Gameboard();
+gameboardPlayer.placeShip(playerDestroyer, { x: 2, y: 3 });
+
+const computerDestroyer = new Ship(2);
+const computerSubmarine = new Ship(3);
+const gameboardComputer = new Gameboard();
+gameboardComputer.placeShip(computerDestroyer, { x: 1, y: 3 });
+gameboardComputer.placeShip(computerSubmarine, { x: 2, y: 1 });
+
 const playerGameboard = document.querySelector(
   "#playerGameboard"
 ) as HTMLDivElement;
-initializeGameboard(playerGameboard);
+initializeGameboard(playerGameboard, gameboardPlayer);
 
 const computerGameboard = document.querySelector(
   "#computerGameboard"
 ) as HTMLDivElement;
-initializeGameboard(computerGameboard);
-
-const playerDestroyer = new Ship(2);
-const gameboardPlayer = new Gameboard();
-gameboardPlayer.placeShip(playerDestroyer, { x: 3, y: 4 });
+initializeGameboard(computerGameboard, gameboardComputer);
